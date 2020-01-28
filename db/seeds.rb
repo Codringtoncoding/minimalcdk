38676ec23ad3a2185f93f3247a9cd70d562cd2e1
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-01-28 15:19:19 +0000

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating issues...'
+Issue.create!({
+  number: "00234542"
+})
+
+Issue.create!({
+  number: "00234543"
+})
+
+
+puts 'Finished!'
